c8d014008f7aa0fc8724b2363c82c04ba25b6e67
Joe-Wehbe <joewehbe@yahoo.com>
2023-08-09 11:19:09 +0300

    Added dependencies and classes

---
+++ ClientWorkspace/ClientWebAPI/src/main/java/net/inmobiles/ClientWebAPI/Name.java
@@ -0,0 +1,51 @@
+package net.inmobiles.ClientWebAPI;
+
+public class Name {
+	
+	private String id;
+	private String fname;
+	private String lname;
+	
+	public Name() {
+		
+	}
+
+	public Name(String id, String fname, String lname) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.lname = lname;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	@Override
+	public String toString() {
+		return "Name [id=" + id + ", fname=" + fname + ", lname=" + lname + "]";
+	}
+
+
+
+}
