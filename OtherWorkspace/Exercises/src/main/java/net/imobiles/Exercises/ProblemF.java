95769ab87c4784026cb2283ee5dc66e2e19e8465
Joe-Wehbe <joewehbe@yahoo.com>
2023-08-18 13:36:50 +0300

    OtherWorkspace/Exercises: implemented a code to load and store file content

--- OtherWorkspace/Exercises/src/main/java/net/imobiles/Exercises/ProblemF.java
+++ OtherWorkspace/Exercises/src/main/java/net/imobiles/Exercises/ProblemF.java
@@ -5,24 +5,57 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ProblemF {
+	
+	private TestCase[] tests = null;
 
 	public void getFileContent() {
 		
-		String path = "C:\\Users\\joewe\\Desktop\\inmobiles\\EclipseWorkspaces\\OtherWorkspace\\Exercises\\src\\main\\java\\net\\imobiles\\Exercises\\input.txt";
-		
+		String path = "C:\\Users\\joewe\\Desktop\\inmobiles\\"
+				+ "EclipseWorkspaces\\OtherWorkspace\\Exercises\\"
+				+ "src\\main\\java\\net\\imobiles\\Exercises\\input.txt";	
 		File file = new File(path);
 		
+		String[] numbers = null;
+		int numOfTests = 0;
+		int count = 1;
+		int index = 0;
+		int n = 0;
+		int k = 0;
+		
 		try {
+			
+			@SuppressWarnings("resource")
 			Scanner scanner = new Scanner(file);
 			
 			System.out.println("The file contains: ");
 			System.out.println("------------------");
-			
+						
 			while(scanner.hasNextLine()) {
+				
 				String[] line = scanner.nextLine().split(" ");
 				for(int i = 0; i < line.length; i++) {
 					System.out.print(line[i]);
+									
+					if(count % 2 == 0) {
+						n = Integer.parseInt(line[0]);
+						k = Integer.parseInt(line[1]);
+					}
+					
+					if(count % 2 == 1) {
+						if(count < 2) {
+							numOfTests = Integer.parseInt(line[0]);
+							tests = new TestCase[numOfTests];
+						}
+						if(count > 2) {
+							numbers = line;
+							tests[index] = new TestCase(n, k, numbers);
+						}
+					}
 				}
+				if(count % 2 == 1 && count > 2) {
+					index++;
+				}
+				count++;
 				System.out.println();
 			}
 			
@@ -31,8 +64,16 @@ public class ProblemF {
 		}
 	}
 	
+	public void print() {
+		System.out.println();
+		for(int i = 0; i < tests.length; i++) {
+			System.out.println(tests[i].toString());
+		}
+	}
+	
 	public static void main(String[] args) {
 		ProblemF pf = new ProblemF();
 		pf.getFileContent();
+		pf.print();
 	}
 }
---
+++ OtherWorkspace/Exercises/src/main/java/net/imobiles/Exercises/TestCase.java
@@ -0,0 +1,46 @@
+package net.imobiles.Exercises;
+
+import java.util.Arrays;
+
+public class TestCase {
+
+	private int n;
+	private int k;
+	private String[] array;
+	
+	public TestCase(int n, int k, String[] array) {
+		this.n = n;
+		this.k = k;
+		this.array = array;
+	}
+
+	public int getN() {
+		return n;
+	}
+
+	public void setN(int n) {
+		this.n = n;
+	}
+
+	public int getK() {
+		return k;
+	}
+
+	public void setK(int k) {
+		this.k = k;
+	}
+
+	public String[] getArray() {
+		return array;
+	}
+
+	public void setArray(String[] array) {
+		this.array = array;
+	}
+
+	@Override
+	public String toString() {
+		return "TestCase [n=" + n + ", k=" + k + ", array=" + Arrays.toString(array) + "]";
+	}
+	
+}
